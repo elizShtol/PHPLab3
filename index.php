36c93ece44d83f85407fb418ae2ef095d5b41402
yourname <yourname@site.com>
2020-12-15 23:09:46 +0500

    Add OOP

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
+namespace App;
 require 'vendor/autoload.php';
+
+$magicClass = new MagicClass();
+$magicClass->CreateSomething();
+$magicClass->DoMagic();
+$magicClass->MakeWonder();
+
+
---
+++ src/MagicClass.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App;
+
+
+class MagicClass
+{
+    public function DoMagic(){
+        echo "DoMagic";
+        echo "<br>";
+    }
+
+    public function MakeWonder(){
+        echo "MakeWonder";
+        echo "<br>";
+    }
+
+    public function CreateSomething(){
+        echo "CreateSomething";
+        echo "<br>";
+    }
+}
